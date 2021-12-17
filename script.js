430063d595e9d50ca22a049796573cc8890199a2
hosie eliase <hosieeliasboy@gmail.com>
2021-12-17 05:32:37 -0500

    dec seventeen

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-const formF = document.querySelector("#form")
+const searchBar = document.querySelectorAll("#input")
+const buttonSearch = document.querySelector("#searchBt")
+const divMatch = document.querySelector(".matches")
+const pawayScore = document.querySelector(".awayscore")
+const phomeScore = document.querySelector(".homescore")
+const form1 = document.querySelector(".form")
+const wcRounds = [];
 
 
-const findTeam = async (input) => {
+async function fetchRounds(){
     try {
-        const searchURL = `https://worldcup.sfg.io/matches/${input}/`
-        const response = await axios.get(searchURL)
-        matcheData(response)
-
-        return response
-    }catch (error){
+        const url = `https://raw.githubusercontent.com/openfootball/worldcup.json/master/2018/worldcup.json`
+        const res = await axios.get(url)
+        let rounds = [res.data.rounds]
+       console.log(res.data.rounds);
+        return res
+        
+    }catch(error){
         console.log(error)
+        console.log("try again")
     }
-}
-const matchesDiv = document.querySelector("#matches");
-
-function matcheData(response){
-   teamObj = response
-   console.log(teamObj)
-   const gameInfo = document.createElement("div")
-   gameInfo.classList = "match-info"
-
-   const teamName = document.createElement("h2")
-   const day = document.createElement("p")
-   const  score = document.createElement("p")
-    
 
 }
+fetchRounds();
+
+function fetchMatchInfo (){
+    let match = form1.value;
+
+    let currentwcRounds = wcRounds(match)
+    console.log(currentwcRounds)
+    removeData();
+
+    let h1 = document.createElement("h1")
+    h1.innerText = `${match}`
+    divMatch.appendChild(h1);
+
+    pawayScore.innerHTML = currentwcRounds.away_score;
+    phomeScore.innerText = currentwcRounds.home_score;
+
+
+}
+
