deaa35f259f217081ac2cc15e1758c9615aeffc2
hosie eliase <hosieeliasboy@gmail.com>
2021-12-23 08:56:38 -0500

    mvp completed

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+// const parse = require("nodemon/lib/cli/parse")
 const searchBar = document.querySelector("#input")
 const buttonSearch = document.querySelector("#searchBt")
 const divMatch = document.querySelector(".matches")
 const pawayScore = document.querySelector(".awayscore")
 const phomeScore = document.querySelector(".homescore")
 const form1 = document.querySelector(".form")
-const wcRounds = [];
+let parsedRounds;
 
 
 async function fetchRounds(){
@@ -12,9 +13,9 @@ async function fetchRounds(){
         const url = `https://raw.githubusercontent.com/openfootball/worldcup.json/master/2018/worldcup.json`
         const res = await axios.get(url)
         let rounds = res.data.rounds
-    //    console.log(res.data.rounds);
+        console.log(rounds);
         populateDropdown(rounds)
-        
+        parsedRounds = rounds
     }catch(error){
         console.log(error)
         console.log("try again")
@@ -41,25 +42,31 @@ function populateDropdown(rounds){
 
 
 function gamesOnMatchday (){
-    let games = searchBar.value
-    let currentRounds = populateDropdown(rounds)[games]
-    console.log(games)
-   let h2 = document.createElement('h2');
-   h2.innerText = currentRounds.team1.name;
-    gameData.appendChild(h2)
+    // let games = searchBar.value
+    // let currentRounds = populateDropdown(rounds)[games]
+    let selectedRound = parsedRounds.find(round => round.name === searchBar.value)
+   
+    let h2 = document.createElement('h2');
+    h2.innerText = selectedRound.matches[0].team1.name;
+    divMatch.appendChild(h2)
+
+        let p = document.createElement('p')
+    p.innerText = selectedRound.matches[0].score1 + "-" + selectedRound.matches[0].score2
+    divMatch.appendChild(p);
+
+    // let p1 = document.createElement('p')
+    // 
+    // divMatch.appendChild(p1);
+     
     let h3 = document.createElement('h3');
-    h3.innerText = currentRounds.team2.name;
-    gameData.appendChild(h3)
-    
-    let p = document.createElement('p')
-    p.innerText = currentRounds.score1
-    gameData.appendChild(p);
+    h3.innerText = selectedRound.matches[0].team2.name;
+    divMatch.appendChild(h3)
 
     async function fetchNewData(){
         removeText();
 
-
     }
 
 }
-searchBar.addEventListene  ("change", gamesOnMatchday);
+searchBar.addEventListener("change", gamesOnMatchday);
+buttonSearch.addEventListener("click", reload, false);
